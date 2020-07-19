1e8946ecc7c787e253452186a6624b9d52448a89
AlexDvorak <doe.jimmie@gmail.com>
2020-07-19 09:30:25 -0500

    initial generic Drivetrain class interface added to allow for use of TalonWrapper in Drivetrain class

--- src/main/kotlin/org/awtybots/hoist/frc/motorcontrol/MotorController.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/motorcontrol/MotorController.kt
@@ -1,7 +1,15 @@
 package org.awtybots.hoist.frc.motorcontrol
 
-internal interface MotorController {
+import com.ctre.phoenix.ErrorCode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.NeutralMode
+
+interface MotorController {
     fun setVoltage(volts: Double)
     fun setRPM(rpm: Double)
     fun getRPM(): Double
+    fun configFactoryDefault(): ErrorCode
+    fun configSelectedFeedbackSensor(dev: FeedbackDevice): ErrorCode
+    fun setNeutralMode(mode: NeutralMode)
+    fun configOpenloopRamp(secondsFromNeutralToFull: Double): ErrorCode
 }
