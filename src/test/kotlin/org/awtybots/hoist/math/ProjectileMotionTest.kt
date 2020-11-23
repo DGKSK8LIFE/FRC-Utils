5bcd85e63087b4022c63fa3e52ec6f43954a92c8
matt-mekha <mtmekha8@gmail.com>
2020-11-22 21:32:12 -0600

    performance test

--- src/test/kotlin/org/awtybots/hoist/math/ProjectileMotionTest.kt
+++ src/test/kotlin/org/awtybots/hoist/math/ProjectileMotionTest.kt
@@ -5,10 +5,24 @@ import org.junit.jupiter.api.Assertions.*
 
 class ProjectileMotionTest() {
     @Test
-    fun test1() {
+    fun functionalityTest() {
         val simulation = Simulation(simulationIterations = 10, ballRadius = 0.09, ballMass = 0.14, launchAngle = 30.0)
         val optimalLaunchVelocity = simulation.findOptimalLaunchVelocity(Position(10.0, 2.0))
-        println()
-        println("optimal launch velocity : ${optimalLaunchVelocity}")
+        println("\noptimal launch velocity : ${optimalLaunchVelocity}")
+    }
+
+    @Test
+    fun speedTest() {
+        val simulation = Simulation(simulationIterations = 10, ballRadius = 0.09, ballMass = 0.14, launchAngle = 30.0)
+        
+        val count = 100
+        val startTime = System.currentTimeMillis()
+        for(i in 1..count) {
+            simulation.findOptimalLaunchVelocity(Position(10.0, 2.0))
+        }
+        val endTime = System.currentTimeMillis()
+
+        val avgTime = (endTime - startTime).toDouble() / count.toDouble()
+        println("\naverage simulation time: ${avgTime}ms")
     }
 }
