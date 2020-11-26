f57a53cd40d828764026fe027671e7288de8d930
Alex Brown <doe.jimmie@gmail.com>
2020-11-26 17:49:58 -0600

    planning complete

--- src/main/kotlin/org/awtybots/hoist/frc/CompetitionBot.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/CompetitionBot.kt
@@ -2,22 +2,38 @@ package org.awtybots.hoist.frc
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-open class CompetitionBot(
+open object CompetitionBot(
     
 ): TimedRobot() {
+
+    // make subsystems public properties of this class?
+    // what else?
+
+    fun addAutonOption(name: String, cmd: Command) {
+    }
   
-    override fun robotInit() { }
+    override fun robotInit() {
+        // create subsystem instances? how? (they're gonna be singletons i know that much
+        // create auton selector on smartdashboard (list of something? enums?)
+    }
 
     override fun robotPeriodic() = CommandScheduler.getInstance().run()
 
-    override fun disabledInit() { }
+    override open fun disabledInit() { /* whatever let them override it */ }
 
     override fun autonomousInit() {
+        // start selected auton command
     }
 
     override fun teleopInit() {
-        
+        // cancel auton command
+        // bind OI
+        // set default command for driving subsytem
     }
 
 }
