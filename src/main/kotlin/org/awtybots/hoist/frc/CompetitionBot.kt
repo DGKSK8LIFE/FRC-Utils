0e7f11e619b35118068d3fdac966df5ff7896ee5
Alex Brown <doe.jimmie@gmail.com>
2020-11-27 21:23:46 -0600

    start auton command on auton init and cancel on teleopInit

--- src/main/kotlin/org/awtybots/hoist/frc/CompetitionBot.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/CompetitionBot.kt
@@ -11,19 +11,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 open class CompetitionBot: TimedRobot() {
 
     private val autonOptions: MutableMap<String, Command> = mutableMapOf()
+    private var autonSelector = SendableChooser<Command>()
+    private var autonCommand: Command? = null
 
-    fun addAutonOption(name: String, cmd: Command) {
-        if (cmd != null && name != null)
-            autonOptions.put(name,cmd)
-    }
+    fun addAutonOption(name: String, cmd: Command) = autonOptions.put(name,cmd)
   
     override fun robotInit() {
-        // create auton selector on smartdashboard (list of something? enums?)
-        var autonSelector = SendableChooser<Command>() // TODO THIS IS BROKEN
-        if (!autonOptions.isEmpty())
-            for ((name,cmd) in autonOptions) {
+        if (autonOptions.isEmpty()) println("No autonomous commands were set")
+        else { // TODO maybe make the first one default?
+            autonOptions.forEach { (name,cmd) ->
                 autonSelector.addOption(name, cmd)
             }
+            SmartDashboard.putData(autonSelector)
+        }
     }
 
     override fun robotPeriodic() = CommandScheduler.getInstance().run()
@@ -32,10 +32,13 @@ open class CompetitionBot: TimedRobot() {
 
     override fun autonomousInit() {
         // start selected auton command
+        autonCommand = autonSelector.getSelected()
+        autonCommand?.schedule()
     }
 
     override fun teleopInit() {
         // cancel auton command
+        autonCommand?.cancel()
         // bind OI
         // set default command for driving subsytem
     }
