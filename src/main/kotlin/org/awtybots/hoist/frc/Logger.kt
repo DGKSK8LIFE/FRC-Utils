b5347101fd17d65bb79aad6d8ff2102079e3a1d1
Tarek Ali <tarekali15@outlook.com>
2020-11-29 14:32:31 -0600

    encapsulation

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,6 +1,6 @@
 package org.awtybots.hoist.frc
 
-class Logger(var writeToFile: Boolean) {
+class Logger(private val writeToFile: Boolean) {
     fun debug(category: String, message: String) = println(log(category, "Debug", message))
 
     fun warn(category: String, message: String) = println(log(category, "Warn", message))
@@ -8,7 +8,7 @@ class Logger(var writeToFile: Boolean) {
     fun error(category: String, message: String) = println(log(category, "Error", message))
 
     private fun log(category: String, mode: String, message: String): String {
-        var date = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+        val date = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
         return "$date -> $category -> $mode -> $message"
     }
 }
