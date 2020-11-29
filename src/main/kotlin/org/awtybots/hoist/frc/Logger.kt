363c691f41f21a54e71780b1e715926223a5c93e
Tarek Ali <tarekali15@outlook.com>
2020-11-29 14:29:32 -0600

    syntax sugar

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,18 +1,13 @@
 package org.awtybots.hoist.frc
 
 class Logger(var writeToFile: Boolean) {
-    fun debug(category: String, message: String) {
-        log(category, "Debug", message)
-    }
+    fun debug(category: String, message: String) = println(log(category, "Debug", message))
 
-    fun warn(category: String, message: String) {
-        log(category, "Warn", message) }
+    fun warn(category: String, message: String) = println(log(category, "Warn", message))
 
-    fun error(category: String, message: String) {
-        log(category, "Error", message)
-    }
+    fun error(category: String, message: String) = println(log(category, "Error", message))
 
-    private fun log(category: String, mode: String, message: String) {
-        println("$category -> $mode -> $message")
+    private fun log(category: String, mode: String, message: String): String {
+        return "$category -> $mode -> $message"
     }
 }
