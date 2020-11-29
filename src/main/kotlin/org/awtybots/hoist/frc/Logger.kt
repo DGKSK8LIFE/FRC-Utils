0f3ff1147a195bc1e258b26d38504d102b06f5f9
Tarek Ali <tarekali15@outlook.com>
2020-11-29 00:30:47 -0600

    almost done; just implemented logging

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,7 +1,16 @@
 package org.awtybots.hoist.frc
 
 class Logger(var writeToFile: Boolean) {
-    fun Log(loggingMode: String) {
-
+    fun Log(loggingMode: String, data: String) {
+        if (writeToFile == false) {
+            when (loggingMode.toLowerCase()) {
+                "debug" -> println("debug: ${data}")
+                "warn" -> println("warn: ${data}")
+                "error" -> println("error: ${data}")
+                else -> {
+                    println("Invalid Logging Mode")
+                }
+            }
+        }
     }
 }
