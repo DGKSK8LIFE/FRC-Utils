c0ab6a65da5e626ce1b6880522e154315ecf8972
Tarek Ali <tarekali15@outlook.com>
2020-11-29 14:56:08 -0600

    imports n stuff

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,5 +1,8 @@
 package org.awtybots.hoist.frc
 
+import java.io.File
+import java.time.LocalDateTime
+
 class Logger(private val writeToFile: Boolean) {
     fun debug(category: String, message: String) = log(category, "Debug", message)
 
@@ -8,12 +11,12 @@ class Logger(private val writeToFile: Boolean) {
     fun error(category: String, message: String) = log(category, "Error", message)
 
     private fun log(category: String, mode: String, message: String) {
-        val date: String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+        val date: String = LocalDateTime.now()
         val outputString: String = "[$date] [$category] [$mode] $message"
         if (!writeToFile) {
             println(outputString)
         } else {
-            File("../../../../../../logs/log.txt").printWriter().use { out -> history.forEach {
+            File("./log.txt").printWriter().use { out -> history.forEach {
                 out.println(outputString)
             } }
         }
