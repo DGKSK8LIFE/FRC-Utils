182c39f400c9b9426f7b2d3e76a045301afd0a77
Tarek Ali <tarekali15@outlook.com>
2020-11-29 00:50:38 -0600

    changed param name

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,12 +1,12 @@
 package org.awtybots.hoist.frc
 
 class Logger(var writeToFile: Boolean) {
-    fun Log(loggingMode: String, data: String) {
+    fun Log(loggingMode: String, message: String) {
         if (writeToFile == false) {
             when (loggingMode.toLowerCase()) {
-                "debug" -> println("debug: ${data}")
-                "warn" -> println("warn: ${data}")
-                "error" -> println("error: ${data}")
+                "debug" -> println("debug: ${message}")
+                "warn" -> println("warn: ${message}")
+                "error" -> println("error: ${message}")
                 else -> {
                     println("Invalid Logging Mode")
                 }
