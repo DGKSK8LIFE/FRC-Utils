6515f0dc38fcb35383ff858ebfbc6837132a09ce
Tarek Ali <tarekali15@outlook.com>
2020-11-29 14:37:43 -0600

    no returns

--- src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
+++ src/main/kotlin/org/awtybots/hoist/frc/Logger.kt
@@ -1,17 +1,16 @@
 package org.awtybots.hoist.frc
 
 class Logger(private val writeToFile: Boolean) {
-    fun debug(category: String, message: String) = println(log(category, "Debug", message))
+    fun debug(category: String, message: String) = log(category, "Debug", message)
 
-    fun warn(category: String, message: String) = println(log(category, "Warn", message))
+    fun warn(category: String, message: String) = log(category, "Warn", message)
 
-    fun error(category: String, message: String) = println(log(category, "Error", message))
+    fun error(category: String, message: String) = log(category, "Error", message)
 
-    private fun log(category: String, mode: String, message: String): String {
+    private fun log(category: String, mode: String, message: String) {
         if (!writeToFile) {
             val date = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
-            return "[$date] [$category] [$mode] $message"
+            println("[$date] [$category] [$mode] $message")
         }
-        return Nothing
     }
 }
