7cbaf856773ed234abda096e344194e76870136f
matt-mekha <mtmekha8@gmail.com>
2021-02-26 23:37:38 -0600

    more motors

--- botplus/src/main/kotlin/org/awtybots/frc/botplus/motors/Motors.kt
+++ botplus/src/main/kotlin/org/awtybots/frc/botplus/motors/Motors.kt
@@ -3,6 +3,7 @@ package org.awtybots.frc.botplus.motors
 import com.ctre.phoenix.motorcontrol.FeedbackDevice
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController
 import com.ctre.phoenix.motorcontrol.can.TalonFX
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
 
 class SimulatedMotorController() : BaseMotorController(0, "Simulated") {
     // TODO add all motor controller features
@@ -49,7 +50,46 @@ val falcon500Specs = MotorSpecs(
     encoderUnitRatio = 10.0 / 2048.0
 )
 
+val pro775Specs = MotorSpecs(
+    freeSpeed = 18730.0,
+    freeCurrent = 0.7,
+    maxPower = 347.0,
+    stallTorque = 0.71,
+    stallCurrent = 134.0,
+    feedbackDevice = FeedbackDevice.CTRE_MagEncoder_Relative,
+    encoderUnitRatio = 10.0 / 4096.0
+)
+
+val bagSpecs = MotorSpecs(
+    freeSpeed = 13180.0,
+    freeCurrent = 1.8,
+    maxPower = 149.0,
+    stallTorque = 0.4,
+    stallCurrent = 53.0,
+    feedbackDevice = FeedbackDevice.CTRE_MagEncoder_Relative,
+    encoderUnitRatio = 10.0 / 4096.0
+)
+
+val miniCimSpecs = MotorSpecs(
+    freeSpeed = 5840.0,
+    freeCurrent = 3.0,
+    maxPower = 215.0,
+    stallTorque = 1.4,
+    stallCurrent = 89.0,
+    feedbackDevice = FeedbackDevice.CTRE_MagEncoder_Relative,
+    encoderUnitRatio = 10.0 / 4096.0
+)
+
 // motors
 
 class Falcon500(id: Int, gearRatio: Double = 1.0) : RealMotor(TalonFX(id), gearRatio, falcon500Specs)
 class SimulatedFalcon500(gearRatio: Double = 1.0) : Motor(gearRatio, falcon500Specs)
+
+class Pro775(id: Int, gearRatio: Double = 1.0) : RealMotor(TalonSRX(id), gearRatio, pro775Specs)
+class SimulatedPro775(gearRatio: Double = 1.0) : Motor(gearRatio, pro775Specs)
+
+class Bag(id: Int, gearRatio: Double = 1.0) : RealMotor(TalonSRX(id), gearRatio, bagSpecs)
+class SimulatedBag(gearRatio: Double = 1.0) : Motor(gearRatio, bagSpecs)
+
+class MiniCim(id: Int, gearRatio: Double = 1.0) : RealMotor(TalonSRX(id), gearRatio, miniCimSpecs)
+class MiniCimSpecs(gearRatio: Double = 1.0) : Motor(gearRatio, miniCimSpecs)
